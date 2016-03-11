b061a53d9c33fcca89ceaede95e00f0fdbdc5162
BrainySalcido <brainy.salcido@gmail.com>
2016-03-10 17:12:53 -0700

    Desarrollo de querys
    1,2,4 y 5

--- lambda-practice/Program.cs
+++ lambda-practice/Program.cs
@@ -12,20 +12,94 @@ namespace lambda_practice
 
             using (var ctx = new DatabaseContext())
             {
+
                 //1. Listar todos los empleados cuyo departamento tenga una sede en Chihuahua
-               
+                var query1 = ctx.Employees
+
+                    .Where(e => e.City.Name == "Chihuahua")
+                    .Select(s => new { s.Id, s.FirstName, s.LastName, s.City });
+
+                foreach (var employee in query1)
+                {
+
+                    Console.WriteLine("{0}  {1}  {2}  {3}",
+                        employee.Id, employee.FirstName, employee.LastName, employee.City.Name);
+
+
+                }
+                Console.ReadKey();
+                Console.Clear();
+
+
 
                 //2. Listar todos los departamentos y el numero de empleados que pertenezcan a cada departamento.
-               
 
+                var query2 = from employee in ctx.Employees
+                             group employee by employee.DepartmentId
+                    into deparmentGroup
+
+
+                             select new
+                             {
+                                 N = deparmentGroup.Select(e => e.Department.Name).FirstOrDefault(),
+                                 C = deparmentGroup.Count()
+                             };
+                foreach (var department in query2)
+                {
+
+                    Console.WriteLine("{0} {1}",
+                     department.N, department.C);
+
+
+                }
+                Console.ReadKey();
+                Console.Clear();
                 //3. Listar todos los empleados remotos. Estos son los empleados cuya ciudad no se encuentre entre las sedes de su departamento.
-             
+
 
                 //4. Listar todos los empleados cuyo aniversario de contratación sea el próximo mes.
-              
 
-                //Listar los 12 meses del año y el numero de empleados contratados por cada mes.
-              
+                var query4 = ctx.Employees
+                    .Where(e => e.HireDate.Month == DateTime.Today.Month + 1);
+                foreach (var employee in query4)
+                {
+                    Console.WriteLine("{0} {1} {2}",
+                      employee.FirstName, employee.LastName, employee.HireDate);
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+
+
+
+
+
+
+
+
+
+                //5.Listar los 12 meses del año y el numero de empleados contratados por cada mes.
+
+                var query5 = from employee in ctx.Employees
+                             group employee by employee.HireDate.Month
+                    into hireGroup
+
+
+                             select new
+                             {
+                                 N = hireGroup.Select(e => e.HireDate).FirstOrDefault(),
+                                 C = hireGroup.Count()
+                             };
+
+
+                foreach (var employee in query5.OrderBy(e => e.N.Month))
+                {
+                    Console.WriteLine("{0} {1}",
+                      employee.N.ToString("MMM"), employee.C);
+                }
+
+                Console.ReadKey();
+                Console.Clear();
 
             }
 
